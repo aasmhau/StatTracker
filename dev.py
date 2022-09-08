a3b4dd062a5d1799da1f53f82d9a9edfc2755331
Asmund Haugen <aasm.hau@gmail.com>
2022-09-08 14:37:58 +0200

    Added graph file for practice

--- dev.py
+++ dev.py
@@ -1,7 +1,8 @@
 import os
+import time
 import msvcrt
+from tkinter import W
 from data import DataStructures as ds
-
 
 class Menu:
 
@@ -11,19 +12,21 @@ class Menu:
     def displayAndPickFromTable(self, optionList: list) -> int:
         os.system('cls')
         asciiInt = 97
+        currentOptions = []
+        print("Esc  -  Cancel")
         for option in optionList:
+            currentOptions.append(option)
             print("{}  -  {}".format(chr(asciiInt), option))
             asciiInt += 1
-        print("Esc  -  Cancel\nBackspace  -  Go back")
 
         selection = int(ord(msvcrt.getch())) - 97
         if selection == -89: # Backspace
-            self.userMenu()
+            return -1
         if selection == -70: # Esc
             self.userMenu()
         while(selection < 0 or selection > len(optionList)-1):
             selection = int(ord(msvcrt.getch())) - 97
-        return optionList[selection] 
+        return currentOptions[selection] 
 
 
     def userMenu(self):
@@ -35,11 +38,38 @@ class Menu:
                     self.registerMatch()
                 case 'Exit':
                     print("Thank you for using this program!")
-                    break
+                    exit(1)
 
 
     def registerMatch(self): # Make it possible to go back with backspace, or cancel with escape
+        print(self.selectMode())
+        time.sleep(3)
+
+    def selectMode(self):
         selectedMode = self.displayAndPickFromTable(ds.maps)
+        if selectedMode == -1:
+            self.registerMatch()
+        return selectedMode + self.selectMap(selectedMode)
+
+    def selectMap(self, mode): 
+        selectedMap = self.displayAndPickFromTable(ds.maps[mode]) 
+        if selectedMap == -1:
+            self.selectMode()
+        return selectedMap + self.selectRole(mode)
+
+    def selectRole(self, mode):
+        selectedRole = self.displayAndPickFromTable(ds.heroes)
+        if selectedRole == -1:
+            self.selectMap(mode)
+        return selectedRole + self.selectHero(selectedRole)
+
+    def selectHero(self, role):
+        selectedHero = self.displayAndPickFromTable(ds.heroes[role])
+        if selectedHero == -1:
+            self.selectRole(role)
+        return selectedHero
+
+
 
 if __name__ == "__main__":
     menu = Menu()
---
+++ graphs.py
@@ -0,0 +1,79 @@
+#BFS
+def bfs(graph,node):
+    visited=[]
+    queue=[]    
+    visited.append(node)
+    queue.append(node)
+    
+    while queue:
+        s=queue.pop(0)
+        
+        for x in graph[s]:
+            if x not in visited:
+                visited.append(x)
+                queue.append(x)
+    return visited
+
+#DFS
+def dfs(graph,node):
+    visited=[]
+    queue=[]
+    
+    queue.append(node)
+    visited.append(node)
+    
+    while queue:
+        s=queue.pop()
+        print(s)
+        for x in graph[s][::-1]:
+            if x not in visited:
+                visited.append(x)
+                queue.append(x)
+                
+
+# Adjascency List representation in Python
+
+
+class AdjNode:
+    def __init__(self, value):
+        self.vertex = value
+        self.next = None
+
+
+class Graph:
+    def __init__(self, num):
+        self.V = num
+        self.graph = [None] * self.V
+
+    # Add edges
+    def add_edge(self, s, d):
+        node = AdjNode(d)
+        node.next = self.graph[s]
+        self.graph[s] = node
+
+        node = AdjNode(s)
+        node.next = self.graph[d]
+        self.graph[d] = node
+
+    # Print the graph
+    def print_agraph(self):
+        for i in range(self.V):
+            print("Vertex " + str(i) + ":", end="")
+            temp = self.graph[i]
+            while temp:
+                print(" -> {}".format(temp.vertex), end="")
+                temp = temp.next
+            print(" \n")
+
+
+if __name__ == "__main__":
+    V = 5
+
+    # Create graph and edges
+    graph = Graph(V)
+    graph.add_edge(0, 1)
+    graph.add_edge(0, 2)
+    graph.add_edge(0, 3)
+    graph.add_edge(1, 2)
+
+    graph.print_agraph()
