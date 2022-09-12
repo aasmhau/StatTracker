8778faaf0244add75604f4cfa65289e4fef7eb95
Asmund Haugen <aasm.hau@gmail.com>
2022-09-12 20:04:33 +0200

    After PEP auto format

--- data.py
+++ data.py
@@ -1,70 +1,69 @@
-from re import I
-
-
 class DataStructures:
-        escort = [  'Circuit Royal', 
-                    'Dorado', 
-                    'Havana', 
-                    'Junkertown', 
-                    'Rialto', 
-                    'Route 66',
-                    'Gothenburg']
 
-        hybrid = [  'Blizzard World', 
-                    'Eichenwalde', 
-                    'Hollywood', 
-                    'King\'s Row', 
-                    'Midtown', 
-                    'Numbani', 
-                    'Paraiso']
+    escort = ['Circuit Royal',
+              'Dorado',
+              'Havana',
+              'Junkertown',
+              'Rialto',
+              'Route 66',
+              'Gothenburg']
 
-        control = [ 'Busan', 
-                    'Ilios', 
-                    'Lijiang Tower', 
-                    'Nepal', 
-                    'Oasis']
+    hybrid = ['Blizzard World',
+              'Eichenwalde',
+              'Hollywood',
+              'King\'s Row',
+              'Midtown',
+              'Numbani',
+              'Paraiso']
 
-        push = [    'Colosseo', 
-                    'New Queen Street', 
-                    'Portugal']
+    control = ['Busan',
+               'Ilios',
+               'Lijiang Tower',
+               'Nepal',
+               'Oasis']
 
-        tank = [    'D.Va', 
-                    'Doomfist',
-                    'Junker Queen', 
-                    'Orisa', 
-                    'Reinhardt', 
-                    'Roadhog', 
-                    'Sigma', 
-                    'Winston', 
-                    'Wrecking Ball', 
-                    'Zarya'] 
+    push = ['Colosseo',
+            'New Queen Street',
+            'Portugal']
 
-        dps = [     'Ashe', 
-                    'Bastion', 
-                    'Cassidy', 
-                    'Echo', 
-                    'Genji', 
-                    'Hanzo', 
-                    'Junkrat', 
-                    'Mei', 
-                    'Phara', 
-                    'Reaper', 
-                    'Soldier: 76', 
-                    'Sojourn', 
-                    'Sombra', 
-                    'Symmetra', 
-                    'Torbjorn', 
-                    'Tracer', 
-                    'Widowmaker'] 
+    tank = ['D.Va',
+            'Doomfist',
+            'Junker Queen',
+            'Orisa',
+            'Reinhardt',
+            'Roadhog',
+            'Sigma',
+            'Winston',
+            'Wrecking Ball',
+            'Zarya']
 
-        support = [ 'Ana', 
-                    'Baptiste', 
-                    'Brigitte', 
-                    'Lucio', 
-                    'Mercy', 
-                    'Moira', 
-                    'Zenyatta']
+    dps = ['Ashe',
+           'Bastion',
+           'Cassidy',
+           'Echo',
+           'Genji',
+           'Hanzo',
+           'Junkrat',
+           'Mei',
+           'Phara',
+           'Reaper',
+           'Soldier: 76',
+           'Sojourn',
+           'Sombra',
+           'Symmetra',
+           'Torbjorn',
+           'Tracer',
+           'Widowmaker']
 
-        maps = {'Escort': escort, 'Hybrid': hybrid, 'Control': control, 'Push': push}
-        heroes = {'Tank': tank, 'DPS': dps, 'Support': support} 
-        results = ['Victory', 'Defeat', 'Draw']
+    support = ['Ana',
+               'Baptiste',
+               'Brigitte',
+               'Lucio',
+               'Mercy',
+               'Moira',
+               'Zenyatta']
+
+    maps = {'Escort': escort, 'Hybrid': hybrid,
+            'Control': control, 'Push': push}
+    heroes = {'Tank': tank, 'DPS': dps, 'Support': support}
+    results = ['Victory', 'Defeat', 'Draw']
--- dev.py
+++ dev.py
@@ -4,10 +4,11 @@ import msvcrt
 from tkinter import W
 from data import DataStructures as ds
 
+
 class Menu:
 
     def __init__(self):
-        self.menuOptions = ['Register Match', 'Exit'] 
+        self.menuOptions = ['Register Match', 'Exit']
 
     def displayAndPickFromTable(self, optionList: list) -> int:
         os.system('cls')
@@ -20,15 +21,14 @@ class Menu:
             asciiInt += 1
 
         selection = int(ord(msvcrt.getch())) - 97
-        if selection == -70: # Esc
+        if selection == -70:  # Esc
             self.userMenu()
-        while(selection < 0 or selection > len(optionList)-1):
+        while (selection < 0 or selection > len(optionList)-1):
             selection = int(ord(msvcrt.getch())) - 97
-        return currentOptions[selection] 
-
+        return currentOptions[selection]
 
     def userMenu(self):
-        while(True):
+        while (True):
             menuSelection = self.displayAndPickFromTable(self.menuOptions)
 
             match menuSelection:
@@ -38,19 +38,18 @@ class Menu:
                     print("Thank you for using this program!")
                     exit(1)
 
-
-    def registerMatch(self): # Make it possible to go back with backspace, or cancel with escape
+    # Make it possible to go back with backspace, or cancel with escape
+    def registerMatch(self):
         selectedMode = self.displayAndPickFromTable(ds.maps)
         selectedMap = self.displayAndPickFromTable(ds.maps[selectedMode])
         selectedRole = self.displayAndPickFromTable(ds.heroes)
         selectedHero = self.displayAndPickFromTable(ds.heroes[selectedRole])
 
-        print("{}-{}-{}-{}".format(selectedMode, selectedMap, selectedRole, selectedHero))
+        print("{}-{}-{}-{}".format(selectedMode,
+              selectedMap, selectedRole, selectedHero))
         time.sleep(2)
 
 
 if __name__ == "__main__":
     menu = Menu()
     menu.userMenu()
-
-
