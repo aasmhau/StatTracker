88351562a870cb382de3562e63a55ab4ccf255d6
Asmund Haugen <aasm.hau@gmail.com>
2022-09-12 20:21:46 +0200

    variable names

--- dev.py
+++ dev.py
@@ -74,25 +74,25 @@ class Menu:
         self.heroes = {'Tank': self.tank, 'DPS': self.dps, 'Support': self.support}
         self.results = ['Victory', 'Defeat', 'Draw']
 
-    def displayAndPickFromTable(self, optionList: list) -> int:
+    def displayAndPickFromTable(self, option_list: list) -> int:
         os.system('cls')
-        asciiInt = 97
-        currentOptions = []
+        ascii_int = 97
+        current_options = []
         print("Esc  -  Cancel")
-        for option in optionList:
-            currentOptions.append(option)
-            print("{}  -  {}".format(chr(asciiInt), option))
+        for option in option_list:
+            current_options.append(option)
+            print("{}  -  {}".format(chr(ascii_int), option))
             asciiInt += 1
 
         selection = int(ord(msvcrt.getch())) - 97
         if selection == -70:  # Esc
             self.userMenu()
-        while (selection < 0 or selection > len(optionList)-1):
+        while (selection < 0 or selection > len(option_list)-1):
             selection = int(ord(msvcrt.getch())) - 97
-        return currentOptions[selection]
+        return current_options[selection]
 
     def userMenu(self):
-        while (True):
+        while True:
             menuSelection = self.displayAndPickFromTable(self.menuOptions)
 
             match menuSelection:
@@ -104,13 +104,13 @@ class Menu:
 
     # Make it possible to go back with backspace, or cancel with escape
     def registerMatch(self):
-        selectedMode = self.displayAndPickFromTable(self.maps)
-        selectedMap = self.displayAndPickFromTable(self.maps[selectedMode])
-        selectedRole = self.displayAndPickFromTable(self.heroes)
-        selectedHero = self.displayAndPickFromTable(self.heroes[selectedRole])
+        selected_mode = self.displayAndPickFromTable(self.maps)
+        selected_map = self.displayAndPickFromTable(self.maps[selected_mode])
+        selected_role = self.displayAndPickFromTable(self.heroes)
+        selected_hero = self.displayAndPickFromTable(self.heroes[selected_role])
 
-        print("{}-{}-{}-{}".format(selectedMode,
-              selectedMap, selectedRole, selectedHero))
+        print("{}-{}-{}-{}".format(selected_mode,
+              selected_map, selected_role, selected_hero))
         time.sleep(2)
 
 
