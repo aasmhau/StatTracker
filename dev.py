159cfb60cab90437bf48ef8f453a63975ae5e5cb
Asmund Haugen <aasm.hau@gmail.com>
2022-09-12 20:06:50 +0200

    fixing more minor syntax errors

--- dev.py
+++ dev.py
@@ -10,6 +10,74 @@ class Menu:
     def __init__(self):
         self.menuOptions = ['Register Match', 'Exit']
 
+    escort = ['Circuit Royal',
+              'Dorado',
+              'Havana',
+              'Junkertown',
+              'Rialto',
+              'Route 66',
+              'Gothenburg']
+
+    hybrid = ['Blizzard World',
+              'Eichenwalde',
+              'Hollywood',
+              'King\'s Row',
+              'Midtown',
+              'Numbani',
+              'Paraiso']
+
+    control = ['Busan',
+               'Ilios',
+               'Lijiang Tower',
+               'Nepal',
+               'Oasis']
+
+    push = ['Colosseo',
+            'New Queen Street',
+            'Portugal']
+
+    tank = ['D.Va',
+            'Doomfist',
+            'Junker Queen',
+            'Orisa',
+            'Reinhardt',
+            'Roadhog',
+            'Sigma',
+            'Winston',
+            'Wrecking Ball',
+            'Zarya']
+
+    dps = ['Ashe',
+           'Bastion',
+           'Cassidy',
+           'Echo',
+           'Genji',
+           'Hanzo',
+           'Junkrat',
+           'Mei',
+           'Phara',
+           'Reaper',
+           'Soldier: 76',
+           'Sojourn',
+           'Sombra',
+           'Symmetra',
+           'Torbjorn',
+           'Tracer',
+           'Widowmaker']
+
+    support = ['Ana',
+               'Baptiste',
+               'Brigitte',
+               'Lucio',
+               'Mercy',
+               'Moira',
+               'Zenyatta']
+
+    maps = {'Escort': escort, 'Hybrid': hybrid,
+            'Control': control, 'Push': push}
+    heroes = {'Tank': tank, 'DPS': dps, 'Support': support}
+    results = ['Victory', 'Defeat', 'Draw']
+
     def displayAndPickFromTable(self, optionList: list) -> int:
         os.system('cls')
         asciiInt = 97
